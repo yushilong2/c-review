4571922e91ffc1edf8ec3e99077029f0814fb454
yushilong2 <2350065497@qq.com>
2020-11-01 22:31:26 +0800

    c语言的学习

---
+++ homework11.1.1/homework11.1.1/11.1.1.c
@@ -0,0 +1,37 @@
+//本题要求痛击给定整数M和N区间内素数的个数并对它们求和
+//输入格式：输入在一行中给出2个正整数M和N（1<=M<=N<=500）
+//输出格式：在一行中顺序输出M和N区间内素数的个数以及它们的和，数字间以空格分隔
+#include<stdio.h>
+int main() 
+{
+	int m, n;
+	int i;
+	int cnt = 0;
+	int sum = 0;
+
+	scanf("%d %d", &m, &n);
+	if (m == 1)
+		m = 2;
+	for (i = m;i <= n;i++)
+	{
+
+			int isPrime = 1;
+			int k;
+			for (k = 2 ; k < i - 1;k++)
+			{
+				if (i % k == 0)
+				{
+					isPrime = 0;
+					break;
+				}
+			}
+//判断i是不是素数
+		if (isPrime)
+		{
+			cnt++;
+			sum += i;
+		}
+	}
+	printf("%d %d\n", cnt, sum);
+	return 0;
+}
