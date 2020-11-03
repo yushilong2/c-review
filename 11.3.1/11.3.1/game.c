0c61ebb57ee3aacd82f8ddfabb1925bb83ed4bde
yushilong2 <2350065497@qq.com>
2020-11-03 22:51:24 +0800

    c语言的学习

---
+++ 11.3.1/11.3.1/game.c
@@ -0,0 +1,41 @@
+#include"game.h"
+
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < row;i++) 
+	{
+		for (j = 0;j < col;j++) 
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < row;i++)
+	{
+		for (j = 0;j < col;j++)
+		{
+			printf(" %c ",board[i][j]);
+			if (j < col - 1)
+				printf("|");
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			for (j = 0;j < col;j++)
+			{
+				printf("---");
+				if (j < col - 1)
+					printf("|");
+			}
+			printf("\n");
+		}
+	}
+
+}
