8e481296372ad1b2a8f6c1db7f024935531597f6
yushilong2 <2350065497@qq.com>
2020-11-14 23:28:23 +0800

    c语言的学习

--- 11_4_2/11_4_2/2.C
+++ 11_4_2/11_4_2/2.C
@@ -1,5 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h>
+#include<errno.h>
 int main()
 {
+	int* p = (int*)calloc(10, sizeof(int));
+	if (p == NULL)
+	{
+		printf("%s\n", strerror(errno));
+	}
 	return 0;
 }
