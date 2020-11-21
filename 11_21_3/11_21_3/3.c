ab9dc6e6d5bfc6b08801c1275246e223662d65e9
yushilong2 <2350065497@qq.com>
2020-11-21 20:48:10 +0800

    c语言的学习

--- 11_21_3/11_21_3/3.c
+++ 11_21_3/11_21_3/3.c
@@ -24,5 +24,17 @@ int main()
 	fclose(pf);
 	pf = NULL;
 
+	//scanf/printf是针对标准输入流/标准输出流的格式化输入/输出语句
+	//fscanf/fprintf是针对所有输入流/所有输出流的格式化输入/输出语句
+	//sscanf是从字符串中读取格式化的数据，sprintf是把格式化的数据输出成(存储到)字符串
+	struct S s2 = { 100,3.14f,"abcdef" };
+	struct S tmp = { 0 };
+	char buf[1024] = { 0 };
+	//把格式化的数据转换成字符串存储到buf
+	sprintf(buf, "%d %f %s", s2.n, s2.score, s2.arr);
+	//从buf中读取格式化的数据到tmp中
+	sscanf(buf, "%d %f %s", &(tmp.n), &(tmp.score), tmp.arr);
+	printf("%d %f %s\n", tmp.n, tmp.score, tmp.arr);
+
 	return 0;
 }
