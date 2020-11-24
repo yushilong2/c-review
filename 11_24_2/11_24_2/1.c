3384971a8ea555a3b9993cff3ca6316a444d5bf6
yushilong2 <2350065497@qq.com>
2020-11-24 22:00:14 +0800

    c语言的学习

--- 11_24_2/11_24_2/1.c
+++ 11_24_2/11_24_2/1.c
@@ -4,6 +4,11 @@
 #define PRINT(X) printf("the value of "#X" is %d\n",X)
 #define CAT(X,Y) X##Y
 #define MAX(X,Y) ((X)>(Y)?(X):(Y))
+#define TEST(x,y) printf("test\n")
+#define SIZEOF(type) sizeof(type)
+#define MALLOC(num,type) malloc(num*sizeof(type))
+#define NUM 100
+#define DEBUG
 
 int main()
 {
@@ -32,6 +37,42 @@ int main()
 	max = MAX(a++, b++);
 	printf("a=%d,b=%d,max=%d\n", a, b, max);//a=11,b=13,max=12
 	//程序会变得很复杂，很难读
+
+	TEST();
+	//每次使用宏的时候，一份宏定义的代码插入到程序中，除非宏比较短，否则可能大幅度增加程序的长度
+	//宏是没办法调试的
+
+	int ret = SIZEOF(int);
+	printf("%d\n", ret);
+
+	int* p = MALLOC(10, int);
+	//int* p = (int*)malloc(10 * sizeof(int));
+
+	printf("%d\n", NUM);
+#undef NUM
+	//printf("%d\n", NUM);//被移除
+
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
+	int i = 0;
+	for (i = 0;i < 10;i++) 
+	{
+		arr[i] = 0;
+#ifdef DEBUG
+	printf("%d ", arr[i]);
+#endif
+	}
+
+#if defined(DEBUG)
+	printf("\nhehe\n");
+#endif
+
+#if 1==1
+	printf("haha\n");
+#elif 2 == 1
+	printf("hehe\n");
+#else
+	printf("hihi\n");
+#endif
 
 	return 0;
 }
